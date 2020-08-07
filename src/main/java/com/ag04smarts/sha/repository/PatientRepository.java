011d0055556ee3c307aec41a421eae659a18dbfb
luka.saravanja67@gmail.com <luka.saravanja67@gmail.com>
2020-08-07 14:46:09 +0200

    [SHA-7][SHA-8] JPA query methods

--- src/main/java/com/ag04smarts/sha/repository/PatientRepository.java
+++ src/main/java/com/ag04smarts/sha/repository/PatientRepository.java
@@ -1,9 +1,21 @@
 package com.ag04smarts.sha.repository;
 
+import java.util.List;
+
 import com.ag04smarts.sha.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    @Query(value = " SELECT * FROM PATIENT p WHERE p.age > 21 AND p.enlistment_date > '2020-01-01' ", nativeQuery = true)
+    List<Patient> findAllPatientsOlderThan21AndEnlistedAfterDate();
+
+    @Query(value = " SELECT * FROM PATIENT p " +
+        "LEFT JOIN patient_medical_record as pmr ON p.patient_medical_record_id = pmr.patient_medical_record_id " +
+        "LEFT JOIN medical_record_symptom as mrs ON pmr.patient_medical_record_id = mrs.patient_medical_record_id " +
+        "LEFT JOIN symptom as s ON mrs.symptom_id = s.symptom_id" +
+        "AND UPPER(s.description) = UPPER('Coughing') OR UPPER(s.description) = UPPER('Fever') ", nativeQuery = true)
+    List<Patient> findAllPatientsWithSymptoms();
 }
