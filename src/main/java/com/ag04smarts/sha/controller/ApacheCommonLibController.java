320274f1a36d8d5c252bba315d7bb3182980447a
luka.saravanja67@gmail.com <luka.saravanja67@gmail.com>
2020-08-20 11:06:06 +0200

    [SHA-1] swagger documentation of endpoints

--- src/main/java/com/ag04smarts/sha/controller/ApacheCommonLibController.java
+++ src/main/java/com/ag04smarts/sha/controller/ApacheCommonLibController.java
@@ -1,7 +1,12 @@
 package com.ag04smarts.sha.controller;
 
 
+import java.lang.reflect.Array;
+
 import com.ag04smarts.sha.utils.MappingConstants;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.apache.commons.collections4.Bag;
 import org.apache.commons.collections4.bag.HashBag;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +16,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Api(description = "This is basic Apache Common Lib example controller")
 @RestController
 @RequestMapping(value = MappingConstants.APACHE)
 public class ApacheCommonLibController {
 
+    @ApiOperation(value = "Get an Array of 'number' which is copied 'copies' times ", response = Array.class)
     @PostMapping
     public ResponseEntity<?> testApacheLib(
-        @RequestParam("number") int number,
-        @RequestParam("copies") int copies
+        @ApiParam(value = "Number which will be printed in array", required = true) @RequestParam("number") int number,
+        @ApiParam(value = "Number of copies of selected number in array", required = true) @RequestParam("copies") int copies
     ) {
         Bag<Integer> bag = new HashBag<>();
 
